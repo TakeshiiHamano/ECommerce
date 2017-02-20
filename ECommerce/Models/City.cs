5c00d8396482f48cb1831b56704c51b095d5dd50
ECommerce <takeshipaulo@gmail.com>
2017-02-20 11:49:47 -0500

    Department and Cities basic CRUD

---
+++ ECommerce/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "the field {0} is required")]
+        [MaxLength(50, ErrorMessage = "the field {0} must be at least {1} characters length")]
+        [Display(Name = "City")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "the field {0} is required")]
+        public int DeparmentId { get; set; }
+
+        public virtual Deparment Deparment { get; set; }
+    }
+}
---
+++ ECommerce/Models/Deparment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Deparment
+    {
+        [Key]
+        public int DeparmentId { get; set; }
+
+        [Required(ErrorMessage = "the field {0} is required")]
+        [MaxLength(50, ErrorMessage = "the field {0} must be at least {1} characters length")]
+        [Display(Name = "Deparment")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
